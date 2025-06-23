1fb63a72e6791ed0c809e06cf298780a79ce5a90
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2025-06-23 15:52:44 +0200

    Monotonicity

--- Mathlib/Algebra/Order/GroupWithZero/InvNonnegClass.lean
+++ Mathlib/Algebra/Order/GroupWithZero/InvNonnegClass.lean
@@ -24,7 +24,9 @@ section InvNonnegClass
 class InvNonneg (α : Type*) extends CommMagma α, MulZeroClass α, MulOneClass α, LinearOrder α,
     Inv α where
   protected inv_le {a b : α} (ha : 0 ≤ a) (hab : 1 ≤ a * b) : a⁻¹ ≤ b
-  protected le_inv {a b : α} (ha : 0 ≤ a) (hb : 0 ≤ b) (hab : a * b ≤ 1) : b ≤ a⁻¹
+  --protected inv_lt {a b : α} (ha : 0 ≤ a) (hab : 1 < a * b) : a⁻¹ < b
+  protected le_inv {a b : α} (ha : 0 < a) (hb : 0 ≤ b) (hab : a * b ≤ 1) : b ≤ a⁻¹
+  --protected lt_inv {a b : α} (ha : 0 < a) (hb : 0 ≤ b) (hab : a * b < 1) : b < a⁻¹
 
 -- Obstacle : definition of the inverse of 0.
 -- Might be interesting to restrict it to positive a, and adding variants (1 < a * b -> a⁻¹ < b...)
@@ -34,23 +36,23 @@ variable [InvNonneg α] {a b : α}
 lemma inv_le (ha : 0 ≤ a) (hab : 1 ≤ a * b) : a⁻¹ ≤ b :=
   InvNonneg.inv_le ha hab
 
-lemma le_inv (ha : 0 ≤ a) (hb : 0 ≤ b) (hab : a * b ≤ 1) : b ≤ a⁻¹ :=
+lemma le_inv (ha : 0 < a) (hb : 0 ≤ b) (hab : a * b ≤ 1) : b ≤ a⁻¹ :=
   InvNonneg.le_inv ha hb hab
 
 lemma mul_lt_one (ha : 0 ≤ a) (hab : b < a⁻¹) : a * b < 1 :=
   by_contra fun h ↦ not_le_of_gt hab (inv_le ha (not_lt.1 h))
 
-lemma one_le_mul (ha : 0 ≤ a) (hb : 0 ≤ b) (hab : a⁻¹ < b) :
+lemma one_le_mul (ha : 0 < a) (hb : 0 ≤ b) (hab : a⁻¹ < b) :
     1 < a * b :=
   by_contra fun h ↦ not_le_of_gt hab (le_inv ha hb (not_lt.1 h))
 
-lemma inv_nonneg [ZeroLEOneClass α] (h : 0 ≤ a) :
+lemma inv_nonneg [ZeroLEOneClass α] (h : 0 < a) :
     0 ≤ a⁻¹ :=
   le_inv h (le_refl 0) ((mul_zero a).symm ▸ zero_le_one)
 
-lemma mono_on_nonneg' [ZeroLEOneClass α] [NeZero (1 : α)] (ha : 0 ≤ a) (hab : a ≤ b) :
+lemma mono_on_nonneg' [ZeroLEOneClass α] [NeZero (1 : α)] (ha : 0 < a) (hab : a ≤ b) :
     b⁻¹ ≤ a⁻¹ := by
-  apply le_inv ha (inv_nonneg (ha.trans hab))
+  apply le_inv ha (inv_nonneg (ha.trans_le hab))
   rw [mul_comm]
   apply (mul_lt_one (inv_nonneg (ha.trans hab)) ?_).le
   sorry

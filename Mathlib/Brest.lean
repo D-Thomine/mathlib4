c25b3a3d399719d6d82d99cc44537e8f6f2143db
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2026-03-27 18:42:22 +0100

    Travail sur les limites

--- Mathlib/Brest.lean
+++ Mathlib/Brest.lean
@@ -14,26 +14,48 @@ Fichier de travail pour exposé à Brest le lundi 30 mars 2026.
 
 namespace Brest
 
-lemma test_pedestre : ∀ x : ℝ, ∃ y : ℝ, x < y := by
-  --intro x
-  --use x + 1
-  --exact lt_add_one x
+/-- Une suite de réels `u` converge vers `l` si `∀ ε > 0, ∃ N, ∀ n ≥ N, |u_n - l| ≤ ε`. -/
+def seq_limit (u : ℕ → ℝ) (l : ℝ) :=
+  ∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| ≤ ε
+
+/-- Une fonction `f : ℝ → ℝ` est continue en `x₀` si
+`∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| ≤ δ ⇒ |f(x) - f(x₀)| ≤ ε`. -/
+def continuous_at (f : ℝ → ℝ) (x₀ : ℝ) :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| ≤ δ → |f x - f x₀| ≤ ε
+
+-- Commencer par une démonstration papier.
+
+lemma limit_comp (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀) (hf : continuous_at f x₀) :
+    seq_limit (f ∘ u) (f x₀) := by
   sorry
 
--- #check lt_add_one
-
-lemma test_tactique : ∀ x : ℝ, ∃ y : ℝ, x < y := by
-  --intro x
-  --use x + 1
-  --simp
+lemma limit_comp1 (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀) (hf : continuous_at f x₀) :
+    seq_limit (f ∘ u) (f x₀) := by
+  --intros ε hε
+  --obtain ⟨δ, δ_pos, Hf⟩ := hf ε hε
+  --obtain ⟨N, Hu⟩ := hu δ δ_pos
+  --use N
+  --intros n hn
+  --apply Hf
+  --apply Hu n hn
   sorry
 
-lemma test_terme_preuve : ∀ x : ℝ, ∃ y : ℝ, x < y :=
-  --fun x ↦ ⟨x+1, lt_add_one x⟩
-  by sorry
+lemma limit_comp2 (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀) (hf : continuous_at f x₀) :
+    seq_limit (f ∘ u) (f x₀) := by
+  intros ε hε
+  obtain ⟨δ, δ_pos, Hf⟩ := hf ε hε
+  obtain ⟨N, Hu⟩ := hu δ δ_pos
+  -- exact ⟨N, fun n hn ↦ (Hf _) (Hu n hn)⟩
+  sorry
 
-lemma test_bibliotheque : ∀ x : ℝ, ∃ y : ℝ, x < y :=
-  -- exists_gt
-  by sorry
+lemma limit_comp3 (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀) (hf : continuous_at f x₀) :
+    seq_limit (f ∘ u) (f x₀) := by
+  intros ε hε
+  obtain ⟨δ, δ_pos, Hf⟩ := hf ε hε
+  obtain ⟨N, Hu⟩ := hu δ δ_pos
+  --use N
+  --solve_by_elim
+  sorry
+
 
 end Brest

62b427c7481845ddbfdb9888ee5f9d389b5473c0
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2026-07-02 16:50:24 +0200

    Add recursive properties for HitTIme

--- Mathlib/Dynamics/Ergodic/HittingTime.lean
+++ Mathlib/Dynamics/Ergodic/HittingTime.lean
@@ -56,7 +56,7 @@ noncomputable section
 
 namespace Recurrence
 
-open Filter Measurable MeasureTheory Nat Set
+open EventuallyEq Filter Measurable MeasurableSet MeasureTheory Nat Set
 
 open Measure
 
@@ -85,7 +85,7 @@ lemma mem_iterate_of_hitTime (h : HitTime f s x ≠ 0) :
     f^[HitTime f s x] x ∈ s :=
   (sInf_mem (nonempty_of_pos_sInf (pos_of_ne_zero h))).2
 
-lemma hitTime_eq_pos_iff {n : ℕ} (h₀ : n ≠ 0) :
+lemma hitTime_eq_pos_iff (f : α → α) (s : Set α) (x : α) {n : ℕ} (h₀ : n ≠ 0) :
     HitTime f s x = n ↔ (∀ k ∈ Ioo 0 n, f^[k] x ∉ s) ∧ f^[n] x ∈ s := by
   simp only [mem_Ioo, and_imp]
   constructor <;> intro h
@@ -97,14 +97,80 @@ lemma hitTime_eq_pos_iff {n : ℕ} (h₀ : n ≠ 0) :
     exact h.1 (HitTime f s x) ht₀ hn (mem_iterate_of_hitTime ht₀.ne.symm)
 
 lemma hitTime_zero_set_eq_iInter :
-    {x | HitTime f s x = 0} = (⋂ k ≠ 0, f^[k] ⁻¹' sᶜ) := by
+    {x | HitTime f s x = 0} = ⋂ k ≠ 0, f^[k] ⁻¹' sᶜ := by
   ext x
   simp [hitTime_eq_zero_iff_forall]
 
 lemma hitTime_pos_set_eq_iInter_inter {n : ℕ} (h : n ≠ 0) :
     {x | HitTime f s x = n} = (⋂ k ∈ Ioo 0 n, f^[k] ⁻¹' sᶜ) ∩ f^[n] ⁻¹' s := by
   ext x
-  simp [hitTime_eq_pos_iff h]
+  simp [hitTime_eq_pos_iff f s x h]
+
+lemma hitTime_image_eq_zero (h : HitTime f s x = 0) :
+    HitTime f s (f x) = 0 := by
+  refine hitTime_eq_zero_iff_forall.2 fun n n₀ ↦ ?_
+  rw [← Function.iterate_succ_apply]
+  exact hitTime_eq_zero_iff_forall.1 h n.succ n.succ_ne_zero
+
+lemma mapsTo_hitTime_zero :
+    MapsTo f { x | HitTime f s x = 0 } { x | HitTime f s x = 0 } :=
+  fun _ h ↦ hitTime_image_eq_zero h
+
+lemma hitTime_ne_zero_of_hitTime_image (h : HitTime f s (f x) ≠ 0) :
+    HitTime f s x ≠ 0 :=
+  fun h₀ ↦ h (hitTime_image_eq_zero h₀)
+
+lemma hitTime_image_ne_zero (h : HitTime f s x ≠ 0) (hx : f x ∉ s) :
+    HitTime f s (f x) ≠ 0 := by
+  rw [hitTime_pos_iff_exists]
+  refine ⟨HitTime f s x - 1, fun xs ↦ ?_, ?_⟩
+  · rw [Nat.sub_eq_zero_iff_le, Nat.le_one_iff_eq_zero_or_eq_one] at xs
+    rcases xs with xs | xs
+    · exact h xs
+    · apply hx
+      rw [← Function.iterate_one f, ← xs]
+      exact mem_iterate_of_hitTime h
+  · rw [← Function.iterate_succ_apply, Nat.succ_eq_add_one, Nat.sub_one_add_one h]
+    exact mem_iterate_of_hitTime h
+
+lemma hitTime_eq_hitTime_image_add_one (h : HitTime f s x ≠ 0) (hx : f x ∉ s) :
+    HitTime f s x = HitTime f s (f x) + 1 := by
+  rw [hitTime_eq_pos_iff f s x ((HitTime f s (f x)).add_one_ne_zero)]
+  simp only [mem_Ioo, and_imp, Function.iterate_succ_apply]
+  constructor
+  · intro k k₀ kx
+    by_cases k₁ : k = 1
+    · rwa [k₁, Function.iterate_one]
+    have key := ((hitTime_eq_pos_iff f s (f x) (hitTime_image_ne_zero h hx)).1 (by rfl)).1 (k - 1)
+    rw [← Function.iterate_succ_apply, Nat.succ_eq_add_one, Nat.sub_one_add_one k₀.ne.symm] at key
+    apply key
+    grind
+  · exact mem_iterate_of_hitTime (hitTime_image_ne_zero h hx)
+
+lemma hitTime_one_eq_preimage :
+    {x | HitTime f s x = 1} = f ⁻¹' s := by
+  ext x
+  simp only [mem_setOf_eq, hitTime_eq_pos_iff f s x one_ne_zero, mem_Ioo, Function.iterate_one,
+    mem_preimage, and_iff_right_iff_imp]
+  exact fun _ _ _ _ ↦ by linarith
+
+lemma hitTime_eq_preimage_inter_compl {n : ℕ} (n₀ : n ≠ 0) :
+    {x | HitTime f s x = n + 1} = f ⁻¹' ({x | HitTime f s x = n} ∩ sᶜ) := by
+  ext x
+  simp only [mem_setOf_eq, preimage_inter, preimage_setOf_eq, preimage_compl, mem_inter_iff,
+    mem_compl_iff, mem_preimage]
+  refine ⟨fun h ↦ ⟨?_, ?_⟩, fun h ↦ ?_⟩
+  · apply Nat.add_right_cancel (m := 1)
+    rw [← h]
+    refine (hitTime_eq_hitTime_image_add_one (by rw [h]; positivity) (fun hx ↦ ?_)).symm
+    have : HitTime f s x = 1 := by
+      rwa [← Set.mem_setOf_eq (p := fun y ↦ HitTime f s y = 1), hitTime_one_eq_preimage]
+    grind
+  · rw [← Function.iterate_one f]
+    apply ((hitTime_eq_pos_iff f s x n.add_one_ne_zero).1 h).1 1
+    simp [Nat.zero_lt_of_ne_zero n₀]
+  · rw [← h.1] at n₀ ⊢
+    exact hitTime_eq_hitTime_image_add_one (hitTime_ne_zero_of_hitTime_image n₀) h.2
 
 /-- `HitMap f s` is the function which to each point `x` associates the point at which the
 positive orbit of `x` first hits `s`. By the convention `sInf ∅ = 0`, if the positive orbit
@@ -153,7 +219,7 @@ lemma _root_.Measurable.hitMap (hf : Measurable f) (hs : MeasurableSet s) :
     Measurable (HitMap f s) := by
   intro t ht
   rw [hitMap_preimage f s t]
-  refine MeasurableSet.iUnion fun n ↦ MeasurableSet.inter ?_ (hf.iterate n ht)
+  refine .iUnion fun n ↦ .inter ?_ (hf.iterate n ht)
   apply ((hf.hitTime hs) (t := {n}) (by measurability)).congr
   rw [← setOf_eq_eq_singleton, preimage_setOf_eq]
 
@@ -161,23 +227,23 @@ lemma _root_.MeasureTheory.Measure.QuasiMeasurePreserving.aemeasurable_hitTime
     (hf : QuasiMeasurePreserving f μ μ) (hs : NullMeasurableSet s μ) :
     AEMeasurable (HitTime f s) μ := by
   obtain ⟨t, s_t, ht, t_μ⟩ := hs.exists_measurable_subset_ae_eq
-  refine ⟨HitTime f t, hf.measurable.hitTime ht, EventuallyEq.iff_eventuallyEq_preimage.2 fun n ↦ ?_⟩
+  refine ⟨HitTime f t, hf.measurable.hitTime ht, .iff_eventuallyEq_preimage.2 fun n ↦ ?_⟩
   by_cases n₀ : n = 0
   · rw [n₀, hitTime_zero_set_eq_iInter, hitTime_zero_set_eq_iInter]
-    refine Filter.EventuallyEq.countable_iInter fun n ↦ ?_
-    exact Filter.EventuallyEq.countable_iInter fun _ ↦ (hf.iterate n).preimage_ae_eq t_μ.compl.symm
+    refine .countable_iInter fun n ↦ .countable_iInter fun _ ↦ ?_
+    exact (hf.iterate n).preimage_ae_eq t_μ.compl.symm
   · rw [hitTime_pos_set_eq_iInter_inter n₀, hitTime_pos_set_eq_iInter_inter n₀]
-    refine Filter.EventuallyEq.inter ?_ ((hf.iterate n).preimage_ae_eq t_μ.symm)
-    refine Filter.EventuallyEq.countable_iInter fun k ↦ ?_
-    exact Filter.EventuallyEq.countable_iInter fun _ ↦ (hf.iterate k).preimage_ae_eq t_μ.compl.symm
+    refine .inter ?_ ((hf.iterate n).preimage_ae_eq t_μ.symm)
+    refine .countable_iInter fun k ↦ .countable_iInter fun _ ↦ ?_
+    exact (hf.iterate k).preimage_ae_eq t_μ.compl.symm
 
 lemma _root_.MeasureTheory.Measure.QuasiMeasurePreserving.aemeasurable_hitMap
     (hf : QuasiMeasurePreserving f μ μ) (hs : NullMeasurableSet s μ) :
     AEMeasurable (HitMap f s) μ := by
   obtain ⟨g, hg, f_g⟩ := hf.aemeasurable_hitTime hs
   refine ⟨fun x ↦ f^[g x] x, fun t ht ↦ ?_, f_g.mono fun x h ↦ ?_⟩
-  · refine MeasurableSet.congr (s := ⋃ n, { x | g x = n } ∩ f^[n] ⁻¹' t) ?_ (by ext x; simp)
-    exact MeasurableSet.iUnion fun n ↦ MeasurableSet.inter (by measurability) (by measurability)
+  · refine .congr (s := ⋃ n, { x | g x = n } ∩ f^[n] ⁻¹' t) ?_ (by ext x; simp)
+    exact .iUnion fun n ↦ .inter (by measurability) (by measurability)
   · rw [HitMap, h]
 
 lemma _root_.Measurable.QuasiMeasurePreserving.hitMap (hf : QuasiMeasurePreserving f μ μ)
@@ -192,39 +258,26 @@ lemma _root_.Measurable.QuasiMeasurePreserving.hitMap (hf : QuasiMeasurePreservi
 
 open scoped ENNReal
 
+/-- Put this lemma somewhere else. -/
+lemma lintegral_indicator_mul_right {t : Set α} (ht : MeasurableSet t) (f g : α → ℝ≥0∞) :
+    ∫⁻ x in t, (f x) * (g x) ∂μ = ∫⁻ x, (f x) * (t.indicator g x) ∂μ := by
+  rw [← lintegral_indicator ht]; congr 1; ext x
+  exact indicator_mul_right t f g
+
 lemma test (u v w : α → ℝ≥0∞) (hf : MeasurePreserving f μ μ) (hs : MeasurableSet s)
     (hu : ∀ (z : α → ℝ≥0∞), ∫⁻ x, (u x) * (z x) ∂μ = ∫⁻ x, (v x) * (z x) ∂μ
       + ∫⁻ x, (u x) * (z (f x)) ∂μ) :
     ∫⁻ x in s, (u x) * (w x) ∂μ = ∫⁻ x in s, (v x) * (w x) ∂μ
       + ∫⁻ x in {y | HitTime f s y = 1} ∩ s, (u x) * (w (f x)) ∂μ
       + ∫⁻ x in {y | HitTime f s y = 1} ∩ sᶜ, (u x) * (w (f x)) ∂μ := by
-  have h₁ {t : Set α} (ht : MeasurableSet t) (z₁ z₂ : α → ℝ≥0∞) :
-    ∫⁻ x in t, (z₁ x) * (z₂ x) ∂μ = ∫⁻ x, (z₁ x) * (t.indicator z₂ x) ∂μ := by
-      rw [← lintegral_indicator ht]; congr 1; ext x
-      exact indicator_mul_right t z₁ z₂
-  rw [h₁ hs u w, hu (s.indicator w), ← h₁ hs v w, add_assoc]
+  rw [lintegral_indicator_mul_right hs u w, hu (s.indicator w),
+    ← lintegral_indicator_mul_right hs v w, add_assoc]
   congr 1
-  simp only [← indicator_comp_right, ← h₁ (hf.measurable hs) u (w ∘ f)]
+  simp only [← indicator_comp_right, ← lintegral_indicator_mul_right (hf.measurable hs) u (w ∘ f),
+    hitTime_one_eq_preimage]
   rw [← lintegral_union]
-  · simp only [Function.comp_apply, inter_union_compl]
-    congr 2
-    rw [hitTime_pos_set_eq_iInter_inter one_ne_zero]
-    simp only [mem_Ioo, Order.lt_one_iff, preimage_compl, Function.iterate_one, right_eq_inter,
-      subset_iInter_iff, and_imp]
-    intro k k₀ k₁
-    linarith
-  · refine MeasurableSet.inter ?_ (by measurability)
-    have : {y | HitTime f s y = 1} = (HitTime f s) ⁻¹' {1} := by
-      ext y; simp
-    rw [this]
-    apply hf.measurable.hitTime hs
-    measurability
+  · simp
+  · exact .inter (hf.measurable hs) (by measurability)
   · exact (Disjoint.mono inter_subset_right inter_subset_right) disjoint_compl_right
-
-/- TODO :
-  Extraire les propriétés de récurrence sur HitTime (du type HitTime f s (f x) = ...), HitMap.
-  En déduire une caractérisation récurrente de {HitTime = n}.
-  Finir test. -/
-
 
 end Recurrence
